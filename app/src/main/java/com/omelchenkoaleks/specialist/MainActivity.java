3a3a70db1d5ed95f1c58dd0d541e300bb325339a
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-01-17 10:04:01 +0200

    Initial commit

---
+++ app/src/main/java/com/omelchenkoaleks/specialist/MainActivity.java
@@ -0,0 +1,20 @@
+package com.omelchenkoaleks.specialist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.google.com/");
+    }
+}
