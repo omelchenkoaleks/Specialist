ae57f08fc980ac033a051d0c2b62ecc64061511e
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-01-17 10:28:46 +0200

    Улучшаем код, чтобы возможное приложение могло обрабатывать свои собственные запросы на отображение web-ресурсов.

--- app/src/main/java/com/omelchenkoaleks/specialist/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/specialist/MainActivity.java
@@ -2,6 +2,7 @@ package com.omelchenkoaleks.specialist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
         webView = findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://www.google.com/");
+
+        webView.setWebViewClient(new SampleWebViewClient());
+    }
+
+    // handle the back button on the device
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
